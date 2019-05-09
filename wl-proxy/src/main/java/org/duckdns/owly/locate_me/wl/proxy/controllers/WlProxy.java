92b001904abea9f07195473731f7b4f8e5c3ae6b
Juri Berlanda <juriberlanda@hotmail.com>
2019-05-09 22:49:36 +0200

    [Java] Mimic the XML_TRIP_REQUEST2 API.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- wl-proxy/src/main/java/org/duckdns/owly/locate_me/wl/proxy/controllers/WlProxy.java
+++ wl-proxy/src/main/java/org/duckdns/owly/locate_me/wl/proxy/controllers/WlProxy.java
@@ -4,12 +4,13 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 import org.duckdns.owly.locate_me.wl.proxy.client.WlClient;
 import org.duckdns.owly.locate_me.wl.proxy.domain.WlLocation;
 
 import javax.inject.Inject;
 
-@Controller("/route")
+@Controller("/ogd_routing")
 public class WlProxy {
 
     @Inject
@@ -20,6 +21,16 @@ public class WlProxy {
         return HttpResponse.ok(wlClient.getRoute(
                 new WlLocation(latOrigin, lonOrigin),
                 new WlLocation(latDestination, lonDestination))
+                .blockingGet())
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET");
+    }
+
+    @Get(value = "XML_TRIP_REQUEST2", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<String> getRoute(
+            @QueryValue("name_origin") String nameOrigin,
+            @QueryValue("name_destination") String nameDestination) {
+        return HttpResponse.ok(wlClient.getRoute(nameOrigin, nameDestination)
                 .blockingGet())
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Methods", "GET");
