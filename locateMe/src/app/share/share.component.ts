172231b1d573344b6888c18707c2f4b68eb4c9d1
Juri Berlanda <juriberlanda@hotmail.com>
2022-05-01 19:50:25 +0200

    [Cleanup] Simplify ShareComponent.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- locateMe/src/app/share/share.component.ts
+++ locateMe/src/app/share/share.component.ts
@@ -1,39 +1,32 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {LinkGeneratorService, Links} from '../service/linkGenerator.service';
-import {Observable, Subject} from 'rxjs';
-import {takeUntil} from 'rxjs/operators';
-import {MePositionState} from '../store/states/app.state';
-import {Select, Store} from '@ngxs/store';
-import {StopLocating} from '../store/actions/position.actions';
+import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable, take } from 'rxjs';
+import { LinkGeneratorService, Links } from '../service/linkGenerator.service';
+import { StopLocating } from '../store/actions/position.actions';
+import { MePositionState } from '../store/states/app.state';
 
 @Component({
   selector: 'app-share',
   templateUrl: './share.component.html',
 })
-export class ShareComponent implements OnDestroy {
+export class ShareComponent {
   display = false;
   links: Links | null = null;
 
   @Select(MePositionState)
   location$!: Observable<GeolocationPosition>;
 
-  private onDestroy$ = new Subject<boolean>();
-
   constructor(
     private linkGeneratorService: LinkGeneratorService,
     private store: Store,
-  ) {}
-
-  ngOnDestroy(): void {
-    this.onDestroy$.next(true);
-    this.onDestroy$.complete();
+  ) {
   }
 
   showDialog() {
     this.display = true;
     this.store.dispatch(new StopLocating());
     this.location$
-      .pipe(takeUntil(this.onDestroy$))
+      .pipe(take(1))
       .subscribe(position => this.links = this.linkGeneratorService.getLinks(position));
   }
 }
