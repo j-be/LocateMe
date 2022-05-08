d4a3d95d1c6e322a0efbc61c69281654264937ac
Juri Berlanda <juriberlanda@hotmail.com>
2022-05-08 16:11:54 +0200

    [Test] Add some more tests for Home- and MapComponent.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- locateMe/src/app/home/home.component.spec.ts
+++ locateMe/src/app/home/home.component.spec.ts
@@ -1,9 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxsModule } from '@ngxs/store';
+import { of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
 import { HomeModule } from './home.module';
+
+const routerSpy = { navigate: jasmine.createSpy('navigate').and.returnValue(Promise.resolve(true)) };
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -15,9 +19,12 @@ describe('HomeComponent', () => {
         RouterTestingModule,
         HomeModule,
         NgxsModule.forRoot()
+      ],
+      providers: [
+        { provide: ActivatedRoute, useValue: { fragment: of('7+8+9'), } },
+        { provide: Router, useValue: routerSpy },
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -29,4 +36,14 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to map', (done) => {
+    component.positionOther$.subscribe(positionOther => {
+      expect(positionOther.coords.latitude).toEqual(7);
+      expect(positionOther.coords.longitude).toEqual(8);
+      expect(positionOther.coords.accuracy).toEqual(9);
+      expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['map']);
+      done();
+    });
+  })
 });
--- locateMe/src/app/map/map.component.spec.ts
+++ locateMe/src/app/map/map.component.spec.ts
@@ -1,9 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
+import { latLng } from 'leaflet';
+import { PositionFound, PositionOther } from '../store/actions/position.actions';
 
 import { MapComponent } from './map.component';
 import { MapModule } from './map.module';
+
+function forgeGeolocation(latitude: number, longitude: number, accuracy: number): GeolocationPosition {
+  return {
+    coords: {
+      latitude,
+      longitude,
+      accuracy,
+      altitude: null,
+      altitudeAccuracy: null,
+      heading: null,
+      speed: null,
+    },
+    timestamp: 0,
+  };
+}
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -28,5 +45,31 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should put Me pin', () => {
+    TestBed.inject(Store).dispatch(new PositionFound(forgeGeolocation(1, 2, 3)));
+    expect((component['me']).marker.getLatLng()).toEqual(latLng({ lat: 1, lng: 2 }));
+    expect((component['me']).accuracy.getRadius()).toEqual(3);
+    expect((component['map'])?.getCenter()).toEqual(latLng({ lat: 1, lng: 2 }));
+  });
+
+  it('should put Other pin', () => {
+    TestBed.inject(Store).dispatch(new PositionOther(forgeGeolocation(4, 5, 6)));
+    expect((component['other']).marker.getLatLng()).toEqual(latLng({ lat: 4, lng: 5 }));
+    expect((component['other']).accuracy.getRadius()).toEqual(6);
+    expect((component['map'])?.getCenter()).toEqual(latLng({ lat: 4, lng: 5 }));
+  });
+
+  it('should pan to both', () => {
+    TestBed.inject(Store).dispatch(new PositionFound(forgeGeolocation(10, 20, 3)));
+    TestBed.inject(Store).dispatch(new PositionOther(forgeGeolocation(40, 50, 6)));
+    expect(component['map']?.getCenter().lat).toBeCloseTo(26, 1);
+    expect(component['map']?.getCenter().lng).toBeCloseTo(35);
+
+    expect(component['map']?.getBounds().getWest()).toBeLessThan(20);
+    expect(component['map']?.getBounds().getEast()).toBeGreaterThan(50);
+    expect(component['map']?.getBounds().getSouth()).toBeLessThan(10);
+    expect(component['map']?.getBounds().getNorth()).toBeGreaterThan(40);
   });
 });
