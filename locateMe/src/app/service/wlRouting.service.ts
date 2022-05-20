2bd7c8fd73ed010d5cda3f57389781467cc53d67
Juri Berlanda <juriberlanda@hotmail.com>
2022-05-20 18:01:30 +0200

    [Cleanup] Rework HTTP params in WlRoutingService.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- locateMe/src/app/service/wlRouting.service.ts
+++ locateMe/src/app/service/wlRouting.service.ts
@@ -3,8 +3,7 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable, retry} from 'rxjs';
 import {environment} from '../../environments/environment';
 
-const url = `${environment.wlRoutingHost}/ogd_routing/XML_TRIP_REQUEST2?` +
-  'locationServerActive=1&type_origin=coord&type_destination=coord&outputFormat=JSON';
+const url = `${environment.wlRoutingHost}/ogd_routing/XML_TRIP_REQUEST2`;
 
 @Injectable()
 export class WlRoutingService {
@@ -18,6 +17,10 @@ export class WlRoutingService {
   getRoute(origin: GeolocationPosition, destination: GeolocationPosition): Observable<any> {
     const options = {
       params: new HttpParams()
+        .set('locationServerActive', '1')
+        .set('type_origin', 'coord')
+        .set('type_destination', 'coord')
+        .set('outputFormat', 'JSON')
         .set('name_origin', WlRoutingService.formatLocation(origin))
         .set('name_destination', WlRoutingService.formatLocation(destination)) };
     return this.http.get(url, options).pipe(
