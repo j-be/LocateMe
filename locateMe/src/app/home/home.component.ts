befa022d7f5f0eb6f49e78ae3dd86245fd25db40
Juri Berlanda <juriberlanda@hotmail.com>
2024-07-22 19:04:56 +0200

    [Style] Adjust to dark theme.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- locateMe/src/app/home/home.component.ts
+++ locateMe/src/app/home/home.component.ts
@@ -9,6 +9,7 @@ import { MePositionState, OtherPositionState } from '../store/states/app.state';
 
 @Component({
   selector: 'app-home',
+  styleUrls: ['./home.component.sass'],
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit, OnDestroy {
