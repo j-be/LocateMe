0c89507d82eb001b0ccdc941e94dd904cdfe59e2
Juri Berlanda <juriberlanda@hotmail.com>
2026-01-06 19:13:57 +0100

    [NG] Get rid of deprecated RouterTestingModule

--- locateMe/src/app/map/map.component.spec.ts
+++ locateMe/src/app/map/map.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { NgxsModule, Store } from '@ngxs/store';
 import { latLng, point } from 'leaflet';
 import { forgeGeolocation } from '../common';
@@ -20,9 +20,11 @@ describe('MapComponent', () => {
       imports: [
         NgxsModule.forRoot(),
 
-        RouterTestingModule,
         MapModule,
-      ]
+      ],
+      providers: [
+        provideRouter([]),
+      ],
     }).compileComponents();
   });
 
