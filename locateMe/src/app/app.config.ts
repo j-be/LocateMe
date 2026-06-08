927315f9faec2bdefc6d0abcbb83ff794738f017
Juri Berlanda <juriberlanda@hotmail.com>
2026-06-08 14:37:26 +0200

    [Prime] Switch to primeuix

--- locateMe/src/app/app.config.ts
+++ locateMe/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
-import Lara from '@primeng/themes/lara'
+import Lara from '@primeuix/themes/lara'
 import { provideStore } from '@ngxs/store';
 import { MessageService } from 'primeng/api';
 
